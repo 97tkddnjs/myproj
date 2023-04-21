1c27874407164be596684f76d4ac9d277d64ad69
97tkddnjs <tkddnjs206@gmail.com>
2023-04-21 14:08:54 +0900

    myproj

---
+++ proj_code/myproj/src/main/java/com/dboard/myproj/common/dto/LoginForm.java
@@ -0,0 +1,15 @@
+package com.dboard.myproj.common.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class LoginForm {
+
+    @NotEmpty
+    private String id; // pk 사용자 id
+
+    @NotEmpty
+    private String pass; // 사용자 비번
+}
