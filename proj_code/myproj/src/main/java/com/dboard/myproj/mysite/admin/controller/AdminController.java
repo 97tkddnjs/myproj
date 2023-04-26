2128d3339ce20eddf85a1f5449fc8875be998934
Lee Sang Won <tkddnjs206@gmail.com>
2023-04-26 23:52:52 +0900

    update

--- proj_code/myproj/src/main/java/com/dboard/myproj/mysite/admin/controller/AdminController.java
+++ proj_code/myproj/src/main/java/com/dboard/myproj/mysite/admin/controller/AdminController.java
@@ -3,8 +3,18 @@ package com.dboard.myproj.mysite.admin.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *  1. 만들것이 전체 화면
+ *  2. search <- post
+ *  3. member detail <- group, 등등~ 뭐 이리 복잡하니...
+ *  4. admin group <- pop up 방식으로다가
+ *
+ *
+ * */
 
 @RequestMapping("/mysite/admin")
 @Controller
@@ -19,8 +29,15 @@ public class AdminController {
         return "mysite/admin/index";
     }
 
+    /**
+     *  회원 관리를 보여주는 URL
+     *  처음 접속시 전체 데이터 보여주어야 함
+     *  admin은 따로 터치 x~
+     */
     @GetMapping("/member")
-    public String memberAdmin() {
+    public String memberAdmin(Model model) {
+
+
 
         return "mysite/admin/adminmember";
     }
---
+++ proj_code/myproj/src/main/java/com/dboard/myproj/mysite/admin/service/AdminService.java
@@ -0,0 +1,20 @@
+package com.dboard.myproj.mysite.admin.service;
+
+import com.dboard.myproj.mysite.admin.dao.AdminDAO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ *
+ * */
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+    private final AdminDAO dao;
+
+
+
+}
